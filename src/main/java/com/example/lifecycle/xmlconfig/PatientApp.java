75956fabea908183675f646213bf4241e972ba65
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2022-05-05 21:22:00 +0530

    Invoked destroy() method using registerShutdownHook()

--- src/main/java/com/example/lifecycle/xmlconfig/PatientApp.java
+++ src/main/java/com/example/lifecycle/xmlconfig/PatientApp.java
@@ -1,13 +1,19 @@
 package com.example.lifecycle.xmlconfig;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class PatientApp {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("lifecycleconfig.xml");
+        // To invoke the destroy() method, we need to use the registerShutdownHook()
+        // This function is only available in the AbstractApplicationContext, so we need
+        // to import this class.
+        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("lifecycleconfig.xml");
         Patient patient = (Patient) ctx.getBean("patient");
 
         System.out.println(patient);
+
+        // Calling this function to tell Application Context to register the destroy() hook
+        ctx.registerShutdownHook();
     }
 }
