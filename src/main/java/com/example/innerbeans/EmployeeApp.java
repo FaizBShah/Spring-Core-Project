e6e7893fe347eb0cf4ce510f56a35cb5774b3f2e
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2022-05-20 22:50:06 +0530

    Scoping in Spring

--- src/main/java/com/example/innerbeans/EmployeeApp.java
+++ src/main/java/com/example/innerbeans/EmployeeApp.java
@@ -6,8 +6,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class EmployeeApp {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("innerbeansconfig.xml");
-        Employee employee = (Employee) ctx.getBean("employee");
 
-        System.out.println(employee);
+        // Spring uses Singleton Scoping by default
+        // which means that no matter how many times
+        // you call a particular bean, it will always return the
+        // same object/instance.
+        Employee employee1 = (Employee) ctx.getBean("employee");
+        System.out.println(employee1.hashCode());
+
+        Employee employee2 = (Employee) ctx.getBean("employee");
+        System.out.println(employee2.hashCode());
     }
 }
